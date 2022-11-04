6508a2774a9d224a14a96787fb0c9a7eab0e8dab
zonemeen <miqilin18@gmail.com>
2022-11-04 14:34:02 +0800

    refactor: 改用actions生成svg

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Netease Recent Profile
+
+<img align="center" width="310" alt="SVG" src="./163.svg" />
---
+++ index.js
@@ -0,0 +1,34 @@
+import { PythonShell } from 'python-shell'
+import { readFileSync, createWriteStream } from 'fs'
+import ejs from 'ejs'
+import fetch from 'node-fetch'
+import logo from './logo.js'
+
+const imageToBase64 = (url) =>
+  fetch(url)
+    .then((r) => r.buffer())
+    .then((buf) => `data:image/png;base64,` + buf.toString('base64'))
+
+const readTemplateFile = () => readFileSync('./template/svg.ejs', 'utf-8')
+
+PythonShell.run('163music.py', { args: ['126764012'] }, async (err, res) => {
+  if (err) throw err
+  const songs = JSON.parse(res).slice(0, 4)
+  const getAllImages = (recentlyPlayedSongs) =>
+    Promise.all(recentlyPlayedSongs.map(({ song }) => imageToBase64(song.al.picUrl)))
+
+  const covers = await getAllImages(songs)
+
+  const templateParams = {
+    recentPlayed: songs.map(({ song }, i) => {
+      return {
+        name: song.name,
+        artist: song.ar.map(({ name }) => name).join('/'),
+        cover: covers[i],
+        logo,
+      }
+    }),
+  }
+  const svgFile = createWriteStream('163.svg')
+  svgFile.write(ejs.render(readTemplateFile(), templateParams))
+})
